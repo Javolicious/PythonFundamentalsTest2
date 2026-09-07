5bc416c0945c261665f9818aba2f802361ced2ed
Javolicious <38119056+Javolicious@users.noreply.github.com>
2026-09-06 19:39:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -73,24 +73,24 @@ def mostrar_home():
 
 def cagarDataset():
     
-uploaded_file = st.file_uploader(
-"Seleccione el archivo CSV",
-type=["csv"]
-)
+    uploaded_file = st.file_uploader(
+    "Seleccione el archivo CSV",
+    type=["csv"]
+    )
 
-if uploaded_file is not None:
-    df = pd.read_csv(uploaded_file)
+    if uploaded_file is not None:
+        df = pd.read_csv(uploaded_file)
 
-    st.success("Archivo cargado correctamente")
+        st.success("Archivo cargado correctamente")
 
-    st.subheader("Vista del dataset")
-    st.dataframe(df.head())
+        st.subheader("Vista del dataset")
+        st.dataframe(df.head())
 
-    st.subheader("Dimensiones")
-    st.write(f"Filas: {df.shape[0]}")
-    st.write(f"Columnas: {df.shape[1]}")
-else:
-    st.warning("Por favor cargue un archivo CSV")
+        st.subheader("Dimensiones")
+        st.write(f"Filas: {df.shape[0]}")
+        st.write(f"Columnas: {df.shape[1]}")
+    else:
+        st.warning("Por favor cargue un archivo CSV")
 
 
 
