e2125ec7e7e1142aecf0ad937ccbe4a6ffc3fa94
Javolicious <38119056+Javolicious@users.noreply.github.com>
2026-09-06 19:26:34 -0500

    Modify file uploader and add title for analysis
    Updated file uploader and added title for FIFA World Cup analysis.

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 import pandas as pd
 import streamlit as st
 
-st.file_uploader("fifa_world_cup_2026_player_performance.csv")
+st.title("Análisis FIFA World Cup 2026")
+archivo = st.file_uploader(
+"Selecciona el archivo FIFA",
+type=["csv"])
+ 
+if archivo is not None:
+st.success("Archivo cargado correctamente")
